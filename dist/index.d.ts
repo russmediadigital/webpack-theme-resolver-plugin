1552882cd49e66d0ea14c213f67c2ad331d80e22
Julian Martin <julian.martin@russmedia.com>
2019-11-13 12:57:06 +0100

    refactor whole plugin
    easier use now.
    Module stuff was deleted
    less Settings
    make path resolving sync
    remove unneeded dependencies

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,18 +1,14 @@
 export interface IThemeResolverPluginOptions {
-    directories?: string[];
-    prefix?: string;
-    module?: string;
-    singlePackage?: boolean;
-    modulePath?: string;
+    directories: string[];
+    prefix: string;
 }
 export declare class ThemeResolverPlugin {
     static defaultOptions: IThemeResolverPluginOptions;
     private options;
     private pathRegex;
     private cache;
-    private chosenResolver;
     constructor(options: IThemeResolverPluginOptions[]);
     apply(resolver: any): void;
-    resolveComponentPath(reqPath: string): Promise<string>;
-    resolveComponentModule(reqPath: string): Promise<string>;
+    resolveComponentPath(reqPath: string, directories: string[]): string | undefined;
+    private getResolver;
 }
